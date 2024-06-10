3f557aaf43cedb0683e12a4ff0e7776202b79355
Le Van Hoai Phong <77775917+levanhoaiphong@users.noreply.github.com>
2024-06-10 17:15:29 +0700

    Delete folder not found

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
-import { CollectionModule } from './collection/collection.module';
 import { CartModule } from './cart/cart.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { CustomerModule } from './customer/customer.module';
 import { CheckoutModule } from './checkout/checkout.module';
 import { OrderModule } from './order/order.module';
-import { ProductModule } from './product/product.module';
-import { CollectionsModule } from './collections/collections.module';
 import { CollectionModule } from './collection/collection.module';
 
 @Module({
-  imports: [AuthModule, ProductModule, CollectionModule, CollectionsModule, OrderModule, CheckoutModule, CustomerModule, DashboardModule, CartModule],
+  imports: [AuthModule, ProductModule, CollectionModule, OrderModule, CheckoutModule, CustomerModule, DashboardModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
