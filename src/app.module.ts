60a75eeaddeee0c2d85aca4dd5a09016c1d7b6ea
Le Van Hoai Phong <77775917+levanhoaiphong@users.noreply.github.com>
2024-06-11 09:54:01 +0700

    Import Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { ProductModule } from './product/product.module';
-import { CartModule } from './cart/cart.module';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { CustomerModule } from './customer/customer.module';
-import { CheckoutModule } from './checkout/checkout.module';
-import { OrderModule } from './order/order.module';
-import { CollectionModule } from './collection/collection.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './modules/products/products.module';
+import { CartsModule } from './modules/carts/carts.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { CollectionsModule } from './modules/collections/collections.module';
+import { DashboardModule } from './modules/dashboard/dashboard.module';
+import { HomeModule } from './modules/home/home.module';
+import { UsersModule } from './modules/users/users.module';
+import { OrdersModule } from './modules/orders/orders.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, ProductModule, CollectionModule, OrderModule, CheckoutModule, CustomerModule, DashboardModule, CartModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), ProductsModule, CartsModule, AuthModule, CollectionsModule, DashboardModule, HomeModule, UsersModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
