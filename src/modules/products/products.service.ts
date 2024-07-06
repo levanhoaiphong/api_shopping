4be2183c72d6f66db1ffa0a188d73d82b20eb07b
Le Van Hoai Phong <77775917+levanhoaiphong@users.noreply.github.com>
2024-07-06 12:43:13 +0700

    Change File Product

--- src/modules/products/products.service.ts
+++ src/modules/products/products.service.ts
@@ -1,7 +1,39 @@
-import { Injectable, Body } from '@nestjs/common';
+import { Injectable, Body, HttpException, InternalServerErrorException, Headers, Get, Post, Delete, Param, Query, Res, Req, HttpStatus } from '@nestjs/common';
+import { SuccessException } from 'src/config/response';
 
 @Injectable()
 export class ProductsService {
-    async createProduct(path, body){
+    async getAllProduct() {
+        throw new SuccessException(HttpStatus.FOUND, "data", "Get Data Success", new Date().toISOString())
+    }
+    async createProduct(body, deader) {
+        const { } = body
+
+    }
+    async updateProduct(body, deader) {
+        const { } = body
+
+    }
+    async deleteProduct(id, header) {
+
+    }
+    async searchProduct(query) {
+
+    }
+    async sortProduct(body) {
+        const { } = body
+
+    }
+    async getProductByProductId(id) {
+
+    }
+    async uploadFile(req) {
+        console.log(req.files)
+    }
+    async createImgByVariantIdAndImgId(body) {
+
+    }
+    async getAllImgByProductId(id) {
+
     }
 }
